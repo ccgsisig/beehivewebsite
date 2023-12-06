cbe70948b77bc5475991b04f634f4150aa7f54d5
ccgsisig <87790430+ccgsisig@users.noreply.github.com>
2023-12-05 18:31:32 -0600

    ???

---
+++ beehive-website/src/screens/Desktop.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import "./style.css";
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <div className="overlap">
+          <div className="overlap-group">
+            <div className="rectangle" />
+            <div className="rectangle-2" />
+            <div className="rectangle-3" />
+            <div className="rectangle-4" />
+            <div className="rectangle-5" />
+            <div className="rectangle-6" />
+            <div className="rectangle-7" />
+            <div className="rectangle-8" />
+            <div className="rectangle-9" />
+            <p className="sustainabeelity">
+              <span className="text-wrapper">Sustaina</span>
+              <span className="span">bee</span>
+              <span className="text-wrapper">lity</span>
+            </p>
+            <div className="text-wrapper-2">UTD Beehive Monitoring</div>
+            <p className="p">
+              Lorem ipsum, or lipsum as it is sometimes known, is dummy text
+              used in laying out print, graphic or web designs. The passage is
+              attributed to an unknown typesetter in the 15th century who is
+              thought to have scrambled parts
+            </p>
+            <div className="text-wrapper-3">Learn more</div>
+            <img
+              className="image"
+              alt="Image"
+              src="https://c.animaapp.com/QfJ12UvR/img/image-6@2x.png"
+            />
+            <img
+              className="img"
+              alt="Image"
+              src="https://c.animaapp.com/QfJ12UvR/img/image-7@2x.png"
+            />
+            <img
+              className="image-2"
+              alt="Image"
+              src="https://c.animaapp.com/QfJ12UvR/img/image-3@2x.png"
+            />
+          </div>
+          <div className="overlap-2">
+            <div className="rectangle-10" />
+            <div className="text-wrapper-4">103</div>
+            <div className="text-wrapper-5">Weight</div>
+            <div className="text-wrapper-6">lbs</div>
+          </div>
+          <div className="overlap-3">
+            <div className="text-wrapper-7">UTD EPICS F23</div>
+            <div className="text-wrapper-8">Designed by</div>
+          </div>
+        </div>
+        <div className="overlap-4">
+          <div className="rectangle-11" />
+          <div className="text-wrapper-9">49°</div>
+          <div className="text-wrapper-10">Humidity</div>
+        </div>
+        <div className="overlap-group-2">
+          <div className="rectangle-12" />
+          <div className="text-wrapper-11">Temperature</div>
+          <div className="text-wrapper-12">49°</div>
+        </div>
+        <div className="image-wrapper">
+          <img
+            className="image-3"
+            alt="Image"
+            src="https://c.animaapp.com/QfJ12UvR/img/image-5@2x.png"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
